f5168a41ac83978a97c1a50df8291afb11e0e28a
Theon Thai <theonthai@gmail.com>
2017-10-04 12:48:33 +1100

    - added url in constants for theon and allan

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,3 +1,8 @@
-const apiurl = 'http://localhost/wda_assignment_01/public';
+// MIN YONG
+//const apiurl = 'http://localhost/wda_assignment_01/public';
+
+//THEON
+const apiurl = 'http://localhost/rmit-laravel/assignment01/public/';
+
 
 export { apiurl };
